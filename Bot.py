3e0760d210b48f6ac563a2e394d171782e921193
unknown <Eduardo - Meta@Eduardo-Meta.metapositiva.corp.br>
2023-03-10 15:45:47 -0300

    first commit

---
+++ Bot.py
@@ -0,0 +1,34 @@
+import top_Team
+import discord
+import requests
+import json
+from HLTV import *
+from bs4 import BeautifulSoup
+
+data_json = requests.get("https://hltv-api.vercel.app/api/teams.json")
+jsonDados = data_json.content
+dados = json.loads(jsonDados)
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        if message.content == '!Top1':
+            await message.channel.send(dados['name'])
+            await message.channel.send(dados['logo'])
+            await message.channel.send(f"Coach: {dados['coach']}")
+            for item in dados['players']:
+                await message.channel.send(item['fullname']) and await message.channel.send(item["image"])
+
+
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.members = True
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('MTA4Mzc2MzMyNzIxOTUzMTgzNg.GmvLlJ.7snp2uK4JWtXY8DRONoznVKNonl5Uq-5pgfCTo')
